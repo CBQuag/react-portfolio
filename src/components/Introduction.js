078c4c282f3624373a37c3576fc5643e67bdebd0
Conrad Quagliaroli <conradbquag@gmail.com>
2023-06-20 10:42:37 -0400

    added fit commit and budget lingo

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -16,7 +16,10 @@ const Introduction = (props) => {
             <div className='intro-images'>
                 <ImageBlock images='sguessr1.png sguessr4.png sguessr6.png' />
                 <ImageBlock images='jsproj1.png jsproj3.png jsproj6.png' />
+                <ImageBlock images='fc1.png fc2.png fc4.png'/>
+                <ImageBlock images='bl1.png bl8.png bl11.png'/>
                 <ImageBlock images='vacay1.png vacay2.png vacay3.png'/>
+                
                 <div className='trailing'>
                 <ImageBlock images='sguessr1.png sguessr4.png sguessr6.png' />
                 <ImageBlock images='jsproj1.png jsproj3.png jsproj6.png' />
